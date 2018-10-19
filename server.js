f74ccea293be2f85a24c430226c781fdce4f4b07
MarchWaden <max.linz@live.com>
2018-10-19 14:24:46 -0600

    got basic server running so I can test stuff

--- server.js
+++ server.js
@@ -2,24 +2,28 @@ const express    = require('express');
 const app        = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const morgan = require('morgan')
-const usersController = require('./controllers/users');
-const planetsController = require('./controllers/planets')
+const morgan = require('morgan');
+//const usersController = require('./controllers/users');
+//const planetsController = require('./controllers/planets');
+
+const port = 3000;
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 app.use(morgan('short'));
-app.use(express.static('static'))
-require('./db/db');
+app.use(express.static('static'));
+
+//require('./db/db');
 
 
-app.use('/users', usersController);
-app.use('/planets', planetsController);
+//app.use('/users', usersController);
+//app.use('/planets', planetsController);
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
 
-app.listen(3001, () => {
+app.listen(port, () => {
   console.log('listening on port 3000');
 })
