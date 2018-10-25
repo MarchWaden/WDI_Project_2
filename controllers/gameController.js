1fb2fe804eab124c35cf19dcf6e03799e5be1694
MarchWaden <max.linz@live.com>
2018-10-25 13:21:58 -0600

    Game now works with database and renders information, planets, and reticle

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -3,6 +3,25 @@ const router  = express.Router();
 const Planets = require('../models/planets');
 const User = require('../models/users');
 
+
+router.get('/planets', async (req, res) => {
+  const planets = await Planets.find({});
+  console.log(planets);
+  let usablePlanets = [];
+  for(i=0;i<planets.length;i++){
+    let user = await User.findById(planets[i].user);
+      let planet = {
+        name: planets[i].name,
+        x: planets[i].x,
+        y: planets[i].y,
+        about: planets[i].about,
+        creator: user.username
+      }
+      usablePlanets.push(planet);
+  }
+  res.send(usablePlanets);
+});
+
 router.get('/', (req, res) => {
     res.render('game/game.ejs');
 });
