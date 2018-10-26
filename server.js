6e0ca8be89cc21a7d0600f60d43a29c5407df9b9
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-10-26 10:26:15 -0600

    merged files

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(session({
   saveUninitialized: true
 }));
  
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
