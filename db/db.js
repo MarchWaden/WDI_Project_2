dde18e192175f3b8a50d6909d626e4038a7a0a08
MarchWaden <max.linz@live.com>
2019-02-15 12:37:45 -0700

    tweaked instructions

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString =process.env.MONGODB_URI || 'mongodb://stellarium:stellarium@localhost/planets';
+const connectionString =process.env.MONGODB_URI || 'mongodb://localhost/planets';
 
 mongoose.connect(connectionString, { useNewUrlParser: true });
 
